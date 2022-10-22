62bd1bd8b4cee84fde34ca564350b1f3a3dc790f
John Patrick Madrigal <jp.madrigal07@gmail.com>
2022-10-22 10:15:49 +0800

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-6xl">
+        Get started with Unork today
+      </h1>
+    </div>
+  )
+}
+
+export default Home
